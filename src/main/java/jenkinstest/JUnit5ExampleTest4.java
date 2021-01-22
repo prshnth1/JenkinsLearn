ace312e87714591708499a972c3fa36b6842bc98
prshnth1 <prshnth1@gmail.com>
2021-01-23 00:24:03 +0530

    Create JUnit5ExampleTest4.java

---
+++ src/main/java/jenkinstest/JUnit5ExampleTest4.java
@@ -0,0 +1,11 @@
+package jenkinstest;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5ExampleTest4 {
+ 
+    @Test
+    void justAnExample() {
+    	System.out.println("we are runnig the test cases2");
+    }
+}
