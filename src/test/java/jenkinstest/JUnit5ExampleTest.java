d50e4d0953235cd0bd75f28055d5b62ea6f29f86
prshnth1 <prshnth1@gmail.com>
2021-01-22 23:21:04 +0530

    Add files via upload

---
+++ src/test/java/jenkinstest/JUnit5ExampleTest.java
@@ -0,0 +1,11 @@
+package jenkinstest;
+
+import org.junit.jupiter.api.Test;
+
+class JUnit5ExampleTest {
+ 
+    @Test
+    void justAnExample() {
+    	System.out.println("we are runnig the test cases");
+    }
+}
